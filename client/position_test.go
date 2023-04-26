b8b5877931c85008e04df3ef830173e7cc52235a
jiang <fenghaojiang97@m.scnu.edu.cn>
2023-04-27 00:34:57 +0800

    chore: remove account holdings

--- client/position_test.go
+++ client/position_test.go
@@ -1,20 +1,15 @@
 package client
 
 import (
-	"context"
 	"testing"
 )
 
 func TestOnAccountHoldings(t *testing.T) {
-	cli, err := NewClientsWithEndpoints([]string{
-		"wss://eth-mainnet.g.alchemy.com/v2/RZpB2x6G6Ls0m3lgRTh66fLwHgeseYSj",
+	_, err := NewClientsWithEndpoints([]string{
+		"https://rpc.ankr.com/eth",
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	holdings, err := cli.GetAccountHoldings(context.Background(), "0xc101c69340FEB4d0c474BF8fC34f5266F3de8A15")
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(holdings)
+
 }
